e09c4efc6a745bad78c6013f47753a4c3c839a6d
Nihal1487 <153359713+Nihal1487@users.noreply.github.com>
2024-01-29 14:14:12 +0530

    import exportt modele added

---
+++ 07_Custom_modele/index.js
@@ -0,0 +1,6 @@
+const { add, sub, multi, name } = require("./oper");
+
+console.log(add(2, 3));
+console.log(sub(3, 2));
+console.log(multi(3, 2));
+console.log(name);
---
+++ 07_Custom_modele/oper.js
@@ -0,0 +1,15 @@
+const add = (a, b) => {
+  return a + b;
+};
+
+const sub = (a, b) => {
+  return a - b;
+};
+
+const multi = (a, b) => {
+  return a * b;
+};
+
+const name = "Nima";
+
+module.exports = { add, sub, multi, name };
