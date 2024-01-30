4ecacf080122768f4a6cfe3458bd3881bef7429b
Nihal1487 <153359713+Nihal1487@users.noreply.github.com>
2024-01-30 11:51:33 +0530

    IIFI Added

---
+++ 09_module_Wrapper_IIFI/index.js
@@ -0,0 +1,13 @@
+// ! dont write code like this bcuz node already do it when you try to write like this then code "FAT GAYA"
+// (function (exports, require, module, _filename, _dirname) {
+
+//   const fs = require("fs");
+//   const name = "Nimahu";
+//   console.log(name);
+//   module.exports = { fs };
+// })();
+
+(function () {
+    const name = "Nima"
+    console.log(name);
+})()
