c795d9a06916fbe3c69046e967a31c7e9f6a6a73
Nihal1487 <153359713+Nihal1487@users.noreply.github.com>
2024-01-31 11:04:41 +0530

    url added /about etc..

--- 10_Http_FristWebServer_nodejs/index.js
+++ 10_Http_FristWebServer_nodejs/index.js
@@ -1,9 +1,20 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-    res.end('Hello World')
+  if (req.url == "/") {
+    res.end("Hello World");
+  } else if (req.url == "/about") {
+    res.end("This is About page");
+  }
+  else if (req.url == "/contact") {
+    res.end("This is contact Us page");
+  }
+  else{
+    res.writeHead(404 ,{"Content-type" : "text/html"})
+    res.end("<h1> 404 Page is not found </h1>")
+  }
 });
 
-server.listen(8000,"127.0.0.1", () => {
-    console.log("App is listening on port 8000");
-})
+server.listen(8000, "127.0.0.1", () => {
+  console.log("App is listening on port 8000");
+});
