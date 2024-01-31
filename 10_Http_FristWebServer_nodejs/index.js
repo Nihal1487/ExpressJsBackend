745208063b9afb5675f484bf16f70ac355d66b86
Nihal1487 <153359713+Nihal1487@users.noreply.github.com>
2024-01-31 12:12:27 +0530

    simple api crated and used

--- 10_Http_FristWebServer_nodejs/index.js
+++ 10_Http_FristWebServer_nodejs/index.js
@@ -1,17 +1,22 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
+  const data = fs.readFileSync(`${__dirname}/UserApi/userdata.json`, "utf-8");
+  const objData = JSON.parse(data);
+
   if (req.url == "/") {
     res.end("Hello World");
   } else if (req.url == "/about") {
     res.end("This is About page");
-  }
-  else if (req.url == "/contact") {
+  } else if (req.url == "/contact") {
     res.end("This is contact Us page");
-  }
-  else{
-    res.writeHead(404 ,{"Content-type" : "text/html"})
-    res.end("<h1> 404 Page is not found </h1>")
+  } else if (req.url == "/userapi") {
+   res.writeHead(200, {"content-type" : "application/json"})
+    res.end(objData.uid);
+  } else {
+    res.writeHead(404, { "Content-type": "text/html" });
+    res.end("<h1> 404 Page is not found </h1>");
   }
 });
 
