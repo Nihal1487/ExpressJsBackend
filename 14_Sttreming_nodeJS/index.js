ee4b86399174eed57084e27372d2d19cb3ddd855
Nihal1487 <153359713+Nihal1487@users.noreply.github.com>
2024-02-01 10:18:54 +0530

    Streme pipe Added

--- 14_Sttreming_nodeJS/index.js
+++ 14_Sttreming_nodeJS/index.js
@@ -4,22 +4,32 @@ const http = require("http");
 const server = http.createServer();
 
 server.on("request", (req, res) => {
-  //   fs.readFile("input.txt", (err, data) => {
+ 
+ // * 1st way
+    //   fs.readFile("input.txt", (err, data) => {
   //     if (err) return console.log(err);
   //     res.end(data.toString());
   //   });
 
-  const rstream = fs.createReadStream("input.txt");
+// * 2nd Way
 
-  rstream.on("data", (chunkData) => {
-    res.write(chunkData);
-  });
-  rstream.on("end", () => {
-    res.end();
-  });
-  rstream.on("error", () => {
-    res.end("file nott found");
-  });
+//   const rstream = fs.createReadStream("input.txt");
+
+//   rstream.on("data", (chunkData) => {
+//     res.write(chunkData);
+//   });
+//   rstream.on("end", () => {
+//     res.end();
+//   });
+//   rstream.on("error", () => {
+//     res.end("file nott found");
+//   });
+
+// * 3rd Way
+
+const rstream = fs.createReadStream("input.txt");
+rstream.pipe(res)
+
 });
 
 server.listen(8000, "127.0.0.1", () => {
