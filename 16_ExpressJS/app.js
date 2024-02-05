d37f0c1846d052e103bafb400eeba08061552002
Nihal1487 <153359713+Nihal1487@users.noreply.github.com>
2024-02-05 13:13:48 +0530

    html and json data added

--- 16_ExpressJS/app.js
+++ 16_ExpressJS/app.js
@@ -3,10 +3,35 @@ const app = express();
 const port = 8000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!!");
+  res.send("<h1>Hello World!!</h1>");
 });
+
 app.get("/about", (req, res) => {
   res.send("Hello About!!");
+});
+
+app.get("/json", (req, res) => {
+  // res.send([
+  //   {
+  //     id: 1,
+  //     name: "Nima",
+  //   },
+  //   {
+  //     id: 2,
+  //     name: "Nimahu",
+  //   },
+  // ]);
+
+  res.json([
+    {
+      id: 1,
+      name: "Nima",
+    },
+    {
+      id: 2,
+      name: "Nimahu",
+    },
+  ]);
 });
 
 app.listen(port, () => {
