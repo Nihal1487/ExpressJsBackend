ede333d346416a1823b2eff39c3f6400f0785125
Nihal1487 <153359713+Nihal1487@users.noreply.github.com>
2024-02-05 12:51:54 +0530

    Express addedddddddddddddddddddddddddddddd

---
+++ 16_ExpressJS/app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const port = 8000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!!");
+});
+app.get("/about", (req, res) => {
+  res.send("Hello About!!");
+});
+
+app.listen(port, () => {
+  console.log(`App is listening on port ${port}`);
+});
